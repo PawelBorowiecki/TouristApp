56944e21543bc439e7921794683341751022f379
PawelBorowiecki <pawelbor77@gmail.com>
2025-01-06 12:34:34 +0100

    findWay button created on MainScreen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.pawel.touristapp"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.pawel.touristapp"
--- app/src/main/java/com/pawel/touristapp/MainActivity.kt
+++ app/src/main/java/com/pawel/touristapp/MainActivity.kt
@@ -1,16 +1,25 @@
 package com.pawel.touristapp
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.pawel.touristapp.ui.theme.TouristAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,8 +28,37 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             TouristAppTheme {
-
+                MainScreen()
             }
         }
+    }
+
+    @Composable
+    private fun MainScreen(){
+        var placeInput by remember { mutableStateOf("") }
+        Column(modifier = Modifier.padding(0.dp, 64.dp)) {
+            TextField(
+                value = placeInput,
+                onValueChange = { placeInput = it },
+                label = { Text("Enter place name") },
+                modifier = Modifier.fillMaxWidth()
+            )
+            Spacer(modifier = Modifier.padding(16.dp))
+            Button(
+                onClick = {
+                    openMap(placeInput)
+                },
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text("Find way")
+            }
+        }
+    }
+
+    private fun openMap(location: String){
+        val intent = Intent(Intent.ACTION_VIEW).apply {
+            data = Uri.parse("geo:0,0?q=$location")
+        }
+        startActivity(intent)
     }
 }
