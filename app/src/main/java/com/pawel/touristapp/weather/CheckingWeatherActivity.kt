f4fbdb8a3a3cf5d633068c3ae5d3132de4ca1aca
PawelBorowiecki <pawelbor77@gmail.com>
2025-02-06 18:18:43 +0100

    WeatherActivity utils refactored

--- app/src/main/java/com/pawel/touristapp/weather/CheckingWeatherActivity.kt
+++ app/src/main/java/com/pawel/touristapp/weather/CheckingWeatherActivity.kt
@@ -8,55 +8,14 @@ import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.annotation.RequiresPermission
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Delete
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.OutlinedCard
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.TileMode
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 import androidx.core.content.ContextCompat
-import androidx.lifecycle.viewmodel.compose.viewModel
 import com.pawel.touristapp.R
-import com.pawel.touristapp.weather.model.WeatherData
-import com.pawel.touristapp.weather.model.WeatherViewModel
+import com.pawel.touristapp.weather.utils.WeatherScreen
 
 class CheckingWeatherActivity : ComponentActivity() {
     companion object{
@@ -70,7 +29,7 @@ class CheckingWeatherActivity : ComponentActivity() {
         enableEdgeToEdge()
         createNotificationChannel()
         setContent {
-            WeatherScreen(receivedLocation)
+            WeatherScreen(receivedLocation, this)
         }
     }
 
@@ -87,7 +46,7 @@ class CheckingWeatherActivity : ComponentActivity() {
         }
     }
 
-    private fun checkNotificationPermission(): Boolean{
+    internal fun checkNotificationPermission(): Boolean{
         return if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU){
             ContextCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS) == PackageManager.PERMISSION_GRANTED
         }else{
@@ -96,7 +55,7 @@ class CheckingWeatherActivity : ComponentActivity() {
     }
 
     @RequiresPermission(Manifest.permission.POST_NOTIFICATIONS)
-    private fun sendNotification(context: Context, location: String){
+    internal fun sendNotification(context: Context, location: String){
         val builer = NotificationCompat.Builder(context, CHANNEL_ID)
             .setSmallIcon(R.drawable.notification_icon)
             .setContentTitle("Saved in database.")
@@ -107,77 +66,5 @@ class CheckingWeatherActivity : ComponentActivity() {
         }
     }
 
-    @Composable
-    private fun WeatherScreen(location: String, viewModel: WeatherViewModel = viewModel()){
-        val weatherList by viewModel.weatherList.collectAsState(initial = emptyList())
 
-        LaunchedEffect(Unit) {
-            viewModel.getWeathers()
-        }
-        viewModel.addWeatherFromApi(
-            cityName = location,
-            apiKey = Constants.OPEN_WEATHER_API_KEY
-        )
-        NotificationToast(
-            context = LocalContext.current,
-            location = location
-        )
-
-        Box(
-            modifier = Modifier.fillMaxSize()
-        ){
-            Image(
-                painter = painterResource(id = R.drawable.weather_activity_background_image),
-                contentDescription = "WeatherActivity background image",
-                contentScale = ContentScale.FillBounds,
-                modifier = Modifier.matchParentSize()
-            )
-            Column(
-                verticalArrangement = Arrangement.Top,
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                LazyColumn(
-                    modifier = Modifier.padding(0.dp, 64.dp, 0.dp, 0.dp).fillMaxSize()
-                ) {
-                    items(weatherList){ weather ->
-                        WeatherItem(weather = weather, onDelete = { viewModel.deleteWeather(weather.id) })
-                    }
-                }
-            }
-        }
-
-
-    }
-
-    @Composable
-    private fun WeatherItem(weather: WeatherData, onDelete: () -> Unit){
-        OutlinedCard(
-            colors = CardDefaults.cardColors(containerColor = Color.Transparent),
-            border = BorderStroke(6.dp, Color.Black),
-            modifier = Modifier.fillMaxWidth().height(250.dp).padding(8.dp, 4.dp, 8.dp, 4.dp).background(brush = Brush.radialGradient(colors = listOf(Color.Transparent, Color.White), tileMode = TileMode.Clamp))
-        ) {
-            Text(text = "City: ${weather.city}", modifier = Modifier.padding(8.dp, 16.dp, 8.dp, 8.dp), color = Color(46, 46, 45), fontSize = 22.sp, fontWeight = FontWeight.Bold)
-            Text(text = "Date: ${weather.date}", modifier = Modifier.padding(8.dp, 16.dp, 8.dp, 8.dp), color = Color(46, 46, 45), fontSize = 20.sp, fontWeight = FontWeight.Bold)
-            Text(text = "Temperature: ${weather.temperature}°C", modifier = Modifier.padding(8.dp, 16.dp, 8.dp, 8.dp), color = Color(46, 46, 45), fontSize = 22.sp, fontWeight = FontWeight.Bold)
-            Text(text = "Description: ${weather.description}", modifier = Modifier.padding(8.dp, 16.dp, 8.dp, 8.dp), color = Color(46, 46, 45), fontSize = 22.sp, fontWeight = FontWeight.Bold)
-
-            IconButton(onClick = onDelete) {
-                Icon(imageVector = Icons.Default.Delete, contentDescription = "Delete", tint = Color.Black)
-            }
-        }
-    }
-
-    @Composable
-    private fun NotificationToast(context: Context, location: String){
-        var permissionGranted by remember { mutableStateOf(checkNotificationPermission()) }
-        val requestPermissionLauncher = rememberLauncherForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
-            permissionGranted = isGranted
-        }
-
-        if(permissionGranted){
-            sendNotification(context, location)
-        }else{
-            requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
-        }
-    }
 }
---
+++ app/src/main/java/com/pawel/touristapp/weather/utils/WeatherScreens.kt
@@ -0,0 +1,125 @@
+package com.pawel.touristapp.weather.utils
+
+import android.Manifest
+import android.content.Context
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedCard
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.TileMode
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.pawel.touristapp.R
+import com.pawel.touristapp.weather.CheckingWeatherActivity
+import com.pawel.touristapp.weather.Constants
+import com.pawel.touristapp.weather.model.WeatherData
+import com.pawel.touristapp.weather.model.WeatherViewModel
+
+@Composable
+fun WeatherScreen(location: String, activity: CheckingWeatherActivity, viewModel: WeatherViewModel = viewModel()){
+    val weatherList by viewModel.weatherList.collectAsState(initial = emptyList())
+
+    LaunchedEffect(Unit) {
+        viewModel.getWeathers()
+    }
+    viewModel.addWeatherFromApi(
+        cityName = location,
+        apiKey = Constants.OPEN_WEATHER_API_KEY
+    )
+    NotificationToast(
+        context = LocalContext.current,
+        location = location,
+        activity = activity
+    )
+
+    Box(
+        modifier = Modifier.fillMaxSize()
+    ){
+        Image(
+            painter = painterResource(id = R.drawable.weather_activity_background_image),
+            contentDescription = "WeatherActivity background image",
+            contentScale = ContentScale.FillBounds,
+            modifier = Modifier.matchParentSize()
+        )
+        Column(
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            LazyColumn(
+                modifier = Modifier.padding(0.dp, 64.dp, 0.dp, 0.dp).fillMaxSize()
+            ) {
+                items(weatherList){ weather ->
+                    WeatherItem(weather = weather, onDelete = { viewModel.deleteWeather(weather.id) })
+                }
+            }
+        }
+    }
+
+
+}
+
+@Composable
+private fun WeatherItem(weather: WeatherData, onDelete: () -> Unit){
+    OutlinedCard(
+        colors = CardDefaults.cardColors(containerColor = Color.Transparent),
+        border = BorderStroke(6.dp, Color.Black),
+        modifier = Modifier.fillMaxWidth().height(250.dp).padding(8.dp, 4.dp, 8.dp, 4.dp).background(brush = Brush.radialGradient(colors = listOf(
+            Color.Transparent, Color.White), tileMode = TileMode.Clamp))
+    ) {
+        Text(text = "City: ${weather.city}", modifier = Modifier.padding(8.dp, 16.dp, 8.dp, 8.dp), color = Color(46, 46, 45), fontSize = 22.sp, fontWeight = FontWeight.Bold)
+        Text(text = "Date: ${weather.date}", modifier = Modifier.padding(8.dp, 16.dp, 8.dp, 8.dp), color = Color(46, 46, 45), fontSize = 20.sp, fontWeight = FontWeight.Bold)
+        Text(text = "Temperature: ${weather.temperature}°C", modifier = Modifier.padding(8.dp, 16.dp, 8.dp, 8.dp), color = Color(46, 46, 45), fontSize = 22.sp, fontWeight = FontWeight.Bold)
+        Text(text = "Description: ${weather.description}", modifier = Modifier.padding(8.dp, 16.dp, 8.dp, 8.dp), color = Color(46, 46, 45), fontSize = 22.sp, fontWeight = FontWeight.Bold)
+
+        IconButton(onClick = onDelete) {
+            Icon(imageVector = Icons.Default.Delete, contentDescription = "Delete", tint = Color.Black)
+        }
+    }
+}
+
+@Composable
+fun NotificationToast(context: Context, location: String, activity: CheckingWeatherActivity){
+    var permissionGranted by remember { mutableStateOf(activity.checkNotificationPermission()) }
+    val requestPermissionLauncher = rememberLauncherForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+        permissionGranted = isGranted
+    }
+
+    if(permissionGranted){
+        activity.sendNotification(context, location)
+    }else{
+        requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
+    }
+}
