ac4d8036015b1c6d36e8bf17e48cbb0d138dd43b
Paweł Borowiecki <161360528+PawelBorowiecki@users.noreply.github.com>
2025-02-23 11:17:56 +0100

    Security update WeatherKey.kt

--- app/src/main/java/com/pawel/touristapp/weather/WeatherKey.kt
+++ app/src/main/java/com/pawel/touristapp/weather/WeatherKey.kt
@@ -1,3 +1,3 @@
 package com.pawel.touristapp.weather
 
-object Constants { const val OPEN_WEATHER_API_KEY = "fdb6ba10117c53c36628367bedd05267" }
+object Constants { const val OPEN_WEATHER_API_KEY = "some6Weather7Key8" }
